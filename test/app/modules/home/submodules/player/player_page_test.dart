e4c8bfe4915168b88f6ea4677d3c5a22f773c14e
unknown <dikdudu@hotmail.com>
2021-01-19 15:05:05 -0300

    removing submodule player

--- test/app/modules/home/submodules/player/player_page_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('PlayerPage has title', (tester) async {
-    //  await tester.pumpWidget(buildTestableWidget(PlayerPage(title: 'Player')));
-    //  final titleFinder = find.text('Player');
-    //  expect(titleFinder, findsOneWidget);
-  });
-}
